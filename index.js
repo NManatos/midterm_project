067d408ba902c4bf76d679107f09e1183cae8c8e
NManatos <NManatos@acg.edu>
2022-06-25 02:03:22 +0300

    Ui changes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var i = 1;
-
+var submit_button = document.querySelector('#submit');
+var artist_input = document.querySelector('#artist');
+var genre_input = document.querySelector('#genre');
+console.log(submit_button,artist_input,genre_input);
 function addRow(tableID) {
   var tbody = document.getElementById(tableID).getElementsByTagName('tbody')[0];
   var table = document.getElementById(tableID);
@@ -7,13 +10,13 @@ function addRow(tableID) {
   var row = table.insertRow(rowCount);
   //inserting values into table
   var cell1 = row.insertCell(0);
-  cell1.innerHTML = document.querySelector('#artist').value;
-  document.querySelector('#artist').value = '';
+  cell1.innerHTML = artist_input.value;
+  artist_input.value = '';
 
 
   var cell2 = row.insertCell(1);
-  cell2.innerHTML = document.querySelector('#genre').value;
-  document.querySelector('#genre').value = '';
+  cell2.innerHTML = genre_input.value;
+  genre_input.value = '';
 
   var cell3 = row.insertCell(2);
   var copynode = document.querySelector("#delete").cloneNode();
@@ -25,6 +28,7 @@ function addRow(tableID) {
   row.id = "row " + i
   tbody.append(row);
   i++;
+  submit_button.disabled = true;
 }
 function deleteRow(deleteNode) {
       try {
@@ -39,3 +43,17 @@ function deleteRow(deleteNode) {
           alert(e);
       }
   }
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+    // By default, submit button is disabled
+    submit_button.disabled = true;
+
+    // Enable button only if there is text in the input field
+    (artist_input&& genre_input).onkeyup = () => {
+        if (artist_input.value.length > 0 && genre_input.value.length > 0)
+          submit_button.disabled = false;
+        else
+          submit_button.disabled = true;
+    };
+  })
